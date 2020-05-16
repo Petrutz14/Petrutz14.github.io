845dd2a8b3e3dd2d4dd6fbd7c848df49bbc9a9a9
Petrutz14 <60011555+Petrutz14@users.noreply.github.com>
2020-05-16 19:01:30 +0300

    Delete site.php

--- site.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <body>
-    <?php
-    $msg=$_POST["message"];
-    $gmail=$_POST["email"];
-    mail("petru.craciunoiu@gmail.com","A messge from a site visitor",$msg);
-    ?>
-  </body>
-</html>
